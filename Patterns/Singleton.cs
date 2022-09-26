dd6b98bc2973df68e99c0f9bae7cf4949a654562
Sohee Park <5000roto@naver.com>
2022-09-26 21:43:18 +0900

    singleton-pattern : ChocolateBoiler 객체 생성

---
+++ Patterns/Singleton.cs
@@ -0,0 +1,43 @@
+public class ChocolateBoiler{
+    private bool empty;
+    private bool boiled;
+    private static ChocolateBoiler uniqueInstance;
+    private ChocolateBoiler(){
+        empty = true;
+        boiled =false;
+    }
+
+    public static ChocolateBoiler getInstance(){
+        if(uniqueInstance == null){
+            uniqueInstance = new ChocolateBoiler();
+        }
+        return uniqueInstance;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain(){
+        if (!isEmpty() && isBoiled()){
+            empty = true;
+        }
+    }
+
+    public void boil(){
+        if (!isEmpty() && !isBoiled()){
+            boiled = true;
+        }
+    }
+
+    public bool isEmpty(){
+        return empty;
+    }
+
+    public bool isBoiled(){
+        return boiled;
+    }
+}
